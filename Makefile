7906b065375651a8edb50deac8b641a8f5a2b0de
Jeff Hammond <jeff.r.hammond@intel.com>
2020-09-30 12:42:33 -0700

    add USM v2 that uses explicit copies

--- Makefile
+++ Makefile
@@ -4,15 +4,12 @@ CXXFLAGS	+= -fsycl
 CXXFLAGS	+= -O3
 EXTFLAGS	 = -fsycl-unnamed-lambda
 
-all: saxpy.x saxpy-usm.x
+all: saxpy.x saxpy-usm.x saxpy-usm2.x
 
-saxpy.x: saxpy.cc
-	$(CXX) $(CXXFLAGS) $< -o $@
-
-saxpy-usm.x: saxpy-usm.cc
+%.x: %.cc
 	$(CXX) $(CXXFLAGS) $< -o $@
 
 clean:
-	-rm -f saxpy.o saxpy.x
-	-rm -f saxpy-usm.o saxpy-usm.x
+	-rm -f *.o
+	-rm -f *.x
 
---
+++ saxpy-usm2.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+
+#include <cmath>
+#include <cfloat>
+
+#include "CL/sycl.hpp"
+
+namespace sycl = cl::sycl;
+
+const float xval(1);
+const float yval(2);
+const float zval(2);
+const float aval(3);
+
+const float correct = (zval + aval * xval + yval);
+
+int main(int argc, char * argv[])
+{
+    if (argc < 2) {
+        std::cerr << "Usage: saxpy-usm2.x <vector length>" << std::endl;
+        return argc;
+    }
+
+    size_t length = std::atoi(argv[1]);
+    std::cout << "SAXPY USMv2 with " << length << " elements" << std::endl;
+
+    // host data
+    std::vector<float> h_X(length,xval);
+    std::vector<float> h_Y(length,yval);
+    std::vector<float> h_Z(length,zval);
+
+    sycl::queue q(sycl::default_selector{});
+
+    auto d_X = sycl::malloc_device<float>(length, q);
+    auto d_Y = sycl::malloc_device<float>(length, q);
+    auto d_Z = sycl::malloc_device<float>(length, q);
+
+    const size_t bytes = length * sizeof(float);
+
+    q.memcpy(d_X, h_X.data(), bytes);
+    q.memcpy(d_Y, h_Y.data(), bytes);
+    q.memcpy(d_Z, h_Z.data(), bytes);
+    q.wait();
+
+    try {
+
+        const float A(aval);
+
+        auto X = d_X;
+        auto Y = d_Y;
+        auto Z = d_Z;
+
+        q.submit([&](sycl::handler& h) {
+            h.parallel_for<class saxpy>( sycl::range<1>{length}, [=] (sycl::id<1> i) {
+                Z[i] += A * X[i] + Y[i];
+            });
+        });
+        q.wait();
+    }
+    catch (sycl::exception & e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+
+    sycl::free(d_X, q);
+    sycl::free(d_Y, q);
+
+    q.memcpy(h_Z.data(), d_Z, bytes);
+    q.wait();
+
+    // check for correctness
+    size_t errors(0);
+    for (size_t i=0; i<length; ++i) {
+        if ( std::abs(h_Z[i] - correct) > FLT_MIN) {
+            ++errors;
+        }
+    }
+    if (errors) {
+        std::cerr << "There were " << errors << " errors!" << std::endl;
+        for (size_t i=0; i<length; ++i) {
+            std::cout << i << "," << h_Z[i] << "\n";
+        }
+        return 1;
+    }
+
+    sycl::free(d_Z, q);
+
+    std::cout << "Program completed without error." << std::endl;
+
+    return 0;
+}
