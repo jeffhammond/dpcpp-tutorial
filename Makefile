66b5cfa52ad700f41aacbcdaccd6eee63a21b918
Jeff Hammond <jeff.r.hammond@intel.com>
2020-09-30 17:15:32 -0700

    add USMv3 with syntax compression

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CXXFLAGS	+= -fsycl
 CXXFLAGS	+= -O3
 EXTFLAGS	 = -fsycl-unnamed-lambda
 
-all: saxpy.x saxpy-usm.x saxpy-usm2.x
+all: saxpy.x saxpy-usm.x saxpy-usm2.x saxpy-usm3.x
 
 %.x: %.cc
 	$(CXX) $(CXXFLAGS) $< -o $@
---
+++ saxpy-usm3.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+
+#include <cmath>
+#include <cfloat>
+
+#include "CL/sycl.hpp"
+
+namespace sycl = cl::sycl;
+
+const float xval(1);
+const float yval(2);
+const float zval(2);
+const float aval(3);
+
+const float correct = (zval + aval * xval + yval);
+
+int main(int argc, char * argv[])
+{
+    if (argc < 2) {
+        std::cerr << "Usage: saxpy-usm2.x <vector length>" << std::endl;
+        return argc;
+    }
+
+    size_t length = std::atoi(argv[1]);
+    std::cout << "SAXPY USMv3 with " << length << " elements" << std::endl;
+
+    // host data
+    std::vector<float> h_X(length,xval);
+    std::vector<float> h_Y(length,yval);
+    std::vector<float> h_Z(length,zval);
+
+    sycl::queue q(sycl::default_selector{});
+
+    auto X = sycl::malloc_device<float>(length, q);
+    auto Y = sycl::malloc_device<float>(length, q);
+    auto Z = sycl::malloc_device<float>(length, q);
+
+    const size_t bytes = length * sizeof(float);
+
+    q.memcpy(X, h_X.data(), bytes);
+    q.memcpy(Y, h_Y.data(), bytes);
+    q.memcpy(Z, h_Z.data(), bytes);
+    q.wait();
+
+    try {
+
+        const float A(aval);
+
+        q.parallel_for<class saxpy>( sycl::range<1>{length}, [=] (sycl::id<1> i) {
+            Z[i] += A * X[i] + Y[i];
+        });
+        q.wait();
+    }
+    catch (sycl::exception & e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+
+    q.memcpy(h_Z.data(), Z, bytes);
+    q.wait();
+
+    // check for correctness
+    size_t errors(0);
+    for (size_t i=0; i<length; ++i) {
+        if ( std::abs(h_Z[i] - correct) > FLT_MIN) {
+            ++errors;
+        }
+    }
+    if (errors) {
+        std::cerr << "There were " << errors << " errors!" << std::endl;
+        for (size_t i=0; i<length; ++i) {
+            std::cout << i << "," << h_Z[i] << "\n";
+        }
+        return 1;
+    }
+
+    sycl::free(X, q);
+    sycl::free(Y, q);
+    sycl::free(Z, q);
+
+    std::cout << "Program completed without error." << std::endl;
+
+    return 0;
+}
