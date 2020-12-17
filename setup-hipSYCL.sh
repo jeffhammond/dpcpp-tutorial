cc8cf9c62e86a3b08e63232b2f826c60eaa8b82b
Hammond, Jeff R <jeff.r.hammond@intel.com>
2020-12-16 16:07:51 -0800

    add hipSYCL

---
+++ setup-hipSYCL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/illuhad/hipSYCL.git && cd hipSYCL
+
+mkdir -p build && cd build
+
+# if the following does not work, look at this file:
+# cat .github/workflows/macos.yml
+
+# to update this, use this:
+# brew list --version llvm
+export BREW_LLVM=/usr/local/Cellar/llvm/11.0.0
+
+cmake .. -DLLVM_DIR=$BREW_LLVM/lib/cmake/llvm -DCLANG_EXECUTABLE_PATH=$BREW_LLVM/bin/clang++
+make -j 2 VERBOSE=ON
